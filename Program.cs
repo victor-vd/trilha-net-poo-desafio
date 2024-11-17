a9c3085a8ef2d89ddf1796ee71a3b4f82b2b5e7a
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-11-17 14:22:24 -0300

    challenge solved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celular1 = new Nokia("940028922", "K15", "8765432123", 128);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("WhatsApp");
+Console.WriteLine("---------------");
+
+Iphone celular2 = new Iphone("912345678", "12", "123456789", 128);
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Telegram");
